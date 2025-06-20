a35952a4affe641b591749143a47baab55fb2bc0
Apostolos Papas <56194423+DevelopWithAP@users.noreply.github.com>
2025-06-20 14:48:44 +0300

    Update theme paths

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,5 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
-import { theme } from "../theme/theme";
+import { theme } from "@/theme/theme";
 
 export default function ProfileSreen() {
   const styles = StyleSheet.create({
